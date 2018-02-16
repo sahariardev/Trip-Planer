7baa928632fc25fbdd79baf63a79b46df4ae4537
sahariardev <sahariaralamk@gmail.com>
2018-02-16 23:58:02 +0600

    Custom exception handler added

--- src/main/java/com/sahariar/TripPlanner/Controller/AddController.java
+++ src/main/java/com/sahariar/TripPlanner/Controller/AddController.java
@@ -1,15 +1,18 @@
 package com.sahariar.TripPlanner.Controller;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sahariar.TripPlanner.Model.Hotel;
 import com.sahariar.TripPlanner.Model.Location;
@@ -38,10 +41,16 @@ public class AddController {
 		return ls.getOne(id);
 	}
 	@PostMapping("/hotel")
-	public void addHotel(@Valid @RequestBody HotelRequest request)
+	public ResponseEntity<Object> addHotel(@Valid @RequestBody HotelRequest request)
 	{
-		hs.addHotelFromRequest(request);
+		Hotel h=hs.addHotelFromRequest(request);
 		
+		URI location=ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(h.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	@GetMapping("/hotel")
 	public List<Hotel> getHotel()
